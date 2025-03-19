6af439dae8ad5174a96978075f1b59852d61085c
Kh0d3Mmd <mohammad84ho@gmail.com>
2025-03-19 12:06:23 +0330

    adding everything for Db

--- Model/FinalProjectDbContext.cs
+++ Model/FinalProjectDbContext.cs
@@ -8,11 +8,21 @@ using System.Threading.Tasks;
 
 namespace Model
 {
-    public class FinalProjectDbContext:DbContext
+    public class FinalProjectDbContext : DbContext
     {
-        public FinalProjectDbContext(DbContextOptions options):base(options)
+        public FinalProjectDbContext(DbContextOptions options) : base(options)
         {
-            
+
+        }
+
+        public FinalProjectDbContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-E5H9JT6;Initial Catalog=FinalProjectDb;Integrated Security=True;MultipleActiveResultSets=true;Encrypt=False;");
         }
         public DbSet<Person> Person { get; set; }
 
